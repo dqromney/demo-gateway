a97c0d65bb0651bda62c594902ecc28b712c9d8a
David Q. Romney <RomneyDQ@ldschurch.org>
2017-05-22 22:43:44 -0600

    Added README.md with instructions.

--- gateway/src/main/java/demo/Application.java
+++ gateway/src/main/java/demo/Application.java
@@ -15,7 +15,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
+    // Add ETag Support
+    // ETags are used to eliminate the need to send a payload to the client when nothing has changed.
+    // The browser is receiving a 304 code instead of 200 for the JavaScript and CSS files since they are unchanged.
     @Bean
     public Filter shallowEtagHeaderFilter() {
         return new ShallowEtagHeaderFilter();
